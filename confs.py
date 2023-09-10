d56736070767d5302f939f57c78c9e96189fcfe0
crazyfacka <gmail@joagonca.com>
2023-09-11 00:54:16 +0100

    Support custom FROM field when forwarding email

--- confs.py
+++ confs.py
@@ -13,6 +13,8 @@ SMTP_PORT = loaded_confs["email"]["smtp"]["port"]
 USERNAME = loaded_confs["email"]["username"]
 PASSWORD = loaded_confs["email"]["password"]
 
+EMAIL_FROM = loaded_confs["email"]["from"]
+
 DB_HOST = loaded_confs["db"]["host"]
 DB_NAME = loaded_confs["db"]["database"]
 DB_USER = loaded_confs["db"]["user"]
--- lib/maildispatcher.py
+++ lib/maildispatcher.py
@@ -8,9 +8,9 @@ from confs import *
 def send_mail(msg, dest):
     """Forward the email based on the FROM address"""
     forward_msg = MIMEMultipart()
-    forward_msg["From"] = USERNAME
+    forward_msg["From"] = EMAIL_FROM
     forward_msg["To"] = dest
-    forward_msg["Subject"] = f"Fwd: {msg['subject']}"
+    forward_msg["Subject"] = msg["subject"]
 
     for part in msg.walk():
         if part.get_content_maintype() == 'multipart':
